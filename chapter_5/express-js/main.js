3e7f6518226b58b46863b83d74930515922d6e66
Raihan Rafid <raihanrfd1@gmail.com>
2023-11-01 20:26:38 +0700

    fix: list with filter name

--- chapter_5/express-js/main.js
+++ chapter_5/express-js/main.js
@@ -11,11 +11,17 @@ app.use(express.json())
 app.get("/api/users", (req, res) => {
   const nameQuery = req.query.name;
 
+  let filteredUsers = listUser;
+
+  if (nameQuery) {
+    filteredUsers = filteredUsers.filter(user => user.name.toLowerCase().includes(nameQuery.toLowerCase()))
+  }
+
   const response = {
     status: "OK",
     message: "Success retrieving data",
     data: {
-      users: listUser.filter(user => user.name.toLowerCase().includes(nameQuery.toLowerCase())),
+      users: filteredUsers,
     },
   }
 
@@ -44,6 +50,23 @@ app.get("/api/users/:id", (req, res) => {
     message: "Success retrieving data",
     data: {
       user: user,
+    },
+  }
+
+  res.status(200).send(response);
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const filteredUsers = listUser.filter(user => user.id !== id);
+  fs.writeFileSync("./data/users.json", JSON.stringify(filteredUsers))
+
+  const response = {
+    status: "OK",
+    message: "Success deleting data",
+    data: {
+      deleted_user: listUser.find(user => user.id === id),
     },
   }
 
@@ -84,6 +107,47 @@ app.post("/api/users", (req, res) => {
   }
 
   res.status(201).send(response);
+});
+
+app.patch("/api/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const payload = req.body;
+
+  // Payload validation
+  if (!payload.name) {
+    const response = {
+      status: "BAD_REQUEST",
+      message: "Name cannot be empty",
+      data: {
+        created_user: null,
+      },
+    }
+
+    res.status(400).send(response);
+  }
+
+  let updatedUser;
+
+  const updatedUsers = listUser.filter((user) => {
+    if (user.id === id) {
+      user.name = payload.name;
+      updatedUser = user;
+    }
+
+    return user
+  })
+
+  fs.writeFileSync("./data/users.json", JSON.stringify(updatedUsers))
+
+  const response = {
+    status: "OK",
+    message: "User succesfully updated",
+    data: {
+      updated_user: updatedUser,
+    },
+  }
+
+  res.status(200).send(response);
 });
 
 const PORT = 8081;
