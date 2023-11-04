d1b399331b89847498fb57076465d68db526ea6d
Raihan Rafid <raihanrfd1@gmail.com>
2023-11-04 13:45:54 +0700

    chap-5: remove unused code

--- chapter_5/express-ts/src/app.ts
+++ chapter_5/express-ts/src/app.ts
@@ -1,14 +1,10 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
+import express, { Application } from 'express';
 import UsersHandler from './handlers/users';
-import AuthMiddleware from './middlewares/auth';
+// import AuthMiddleware from './middlewares/auth';
 import uploadFileUtil from './utils/uploadFile';
-// import formData from 'express-form-data';
 
 const app: Application = express();
 const PORT: number = 8081;
-
-// Add middleware to get the body from the request
-// app.use(express.json());
 
 // Init handlers
 const usersHandler = new UsersHandler();
